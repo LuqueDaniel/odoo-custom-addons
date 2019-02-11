6c3f36b7548954edf52ca2901a699c8247d2f9c9
Daniel Luque <danielluque14@gmail.com>
2019-02-11 21:52:24 +0100

    Fix a conditional logic bug on account _invoice_financial_risk_filter

--- account_invoice_financial_risk_filter/tests/test_account_invoice_financial_risk_filter.py
+++ account_invoice_financial_risk_filter/tests/test_account_invoice_financial_risk_filter.py
@@ -8,9 +8,9 @@ from odoo.tests import common
 @common.post_install(True)
 class TestAccountInvoiceFinancialRiskFilter(common.TransactionCase):
     def test_account_invoice(self):
-        if not 'credit_policy' in self.env['account.invoice']._fields:
+        if 'credit_policy' not in self.env['account.invoice']._fields:
             raise Exception("`credit_policy` does not exist")
 
     def test_account_invoice_report(self):
-        if not 'credit_policy' in self.env['account.invoice.report']._fields:
+        if 'credit_policy' not in self.env['account.invoice.report']._fields:
             raise Exception("`credit_policy` does not exist")
