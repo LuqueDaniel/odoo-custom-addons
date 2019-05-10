d10eb55cfc67a26724fdb9855f6701015f5bb31e
Daniel Luque <danielluque14@gmail.com>
2019-05-11 01:09:13 +0200

    Migrate hide_cost_margin to Odoo 11.0

--- hide_cost_margin/__manifest__.py
+++ hide_cost_margin/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017-2018 Daniel Luque
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     'name': 'Hide costs and margins',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'summary': "Hides the costs and margins fields of some views",
     'author': 'Daniel Luque',
     'website': "https://github.com/LuqueDaniel/odoo-custom-addons",
@@ -15,5 +14,5 @@
         'security/groups.xml',
         'views/sale.xml'
     ],
-    'installable': False
+    'installable': True
 }
