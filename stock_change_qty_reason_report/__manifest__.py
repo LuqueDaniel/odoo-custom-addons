6aabe26903abb4832027b363e74aada6b8492a54
Daniel Luque <danielluque14@gmail.com>
2021-08-06 23:34:09 +0200

    stock_report_qty_reason_report: adds filter by in progress

--- stock_change_qty_reason_report/__manifest__.py
+++ stock_change_qty_reason_report/__manifest__.py
@@ -8,7 +8,7 @@
     "summary": "Adds stock change reason report.",
     "website": "https://github.com/LuqueDaniel/odoo-custom-addons",
     "category": "Stock / Report",
-    "version": "12.0.1.0.1",
+    "version": "12.0.1.0.3",
     "depends": ["stock_change_qty_reason"],
     "data": ["report/stock_reason_report_view.xml", "security/ir.model.access.csv"],
     "installable": True,
