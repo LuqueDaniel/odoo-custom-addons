e8e448ab408e5a48b752394a57ac3c65673d3a61
Daniel Luque <danielluque14@gmail.com>
2025-12-08 20:43:50 +0100

    Migrates hide_cost_margin to Odoo 13.0

---
+++ hide_cost_margin/README.md
@@ -0,0 +1,8 @@
+# Hide costs and margins
+
+This addon hides the cost and margin prices for all users who don't have the "Show costs
+and margins" permission. By default the administrator user has the permission.
+
+## Depends on
+
+- `sale_margin`
---
+++ hide_cost_margin/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2017-2018 Daniel Luque
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Hide costs and margins",
+    "version": "13.0.1.0.0",
+    "summary": "Hides the costs and margins fields of some views",
+    "author": "Daniel Luque",
+    "website": "https://github.com/LuqueDaniel/odoo-custom-addons",
+    "license": "AGPL-3",
+    "category": "Sales",
+    "depends": ["sale_margin", "sale", "purchase"],
+    "data": ["security/groups.xml", "views/sale.xml"],
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+}
