203490000f9f2092acfd057f9c5d845bc0491a63
Daniel Luque <danielluque14@gmail.com>
2025-12-18 20:02:10 +0100

    jstock_change_qty_reason_required: migrate to Odoo 13.0

---
+++ stock_change_qty_reason_required/README.md
@@ -0,0 +1,7 @@
+# Stock Change Quantity Reason Required
+
+Makes the `preset_reason_id` field required.
+
+## Depends on
+
+- [stock_change_qty_reason](https://github.com/OCA/stock-logistics-warehouse/tree/13.0/stock_change_qty_reason)
---
+++ stock_change_qty_reason_required/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Daniel Luque
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Stock Change Quantity Reason Required",
+    "version": "13.0.1.0.0",
+    "author": "Daniel Luque",
+    "license": "AGPL-3",
+    "summary": "Makes the preset reason field required.",
+    "website": "https://github.com/LuqueDaniel/odoo-custom-addons",
+    "category": "Stock",
+    "depends": ["stock_change_qty_reason"],
+    "data": ["views/stock_inventory_view.xml", "views/stock_inventory_line_view.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
